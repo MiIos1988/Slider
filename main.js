e769fdf8e2fd76f4f3c0f501d56a73250fe90565
Milos1988 <vojvoda1988@gmail.com>
2023-09-09 00:33:01 +0200

    Corected code .

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const mainImg = document.querySelector(".slider")
 const allImg = document.querySelectorAll(".box > img")
 const container = document.querySelector(".container")
 const arrow = document.querySelector(".arrow")
-
-
 let counter = 1
 
 btnRight.addEventListener("click", ()=>{
@@ -16,7 +14,6 @@ btnRight.addEventListener("click", ()=>{
 mainImg.setAttribute("src", `img/img${counter}.jpg`)
 allImg.forEach(img=>img.classList.remove("border"))
 allImg[counter-1].classList.add("border")
-
 }) 
 
 btnLeft.addEventListener("click", ()=>{
